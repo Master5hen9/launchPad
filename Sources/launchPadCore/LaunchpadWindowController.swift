70dd5d3158b0c1c56f9d71d0640466d859e92a83
Min9Yu3 <ming8969@163.com>
2026-08-10 23:16:17 +0800

    style: revert nav bar to natural height

--- Sources/launchPadCore/LaunchpadWindowController.swift
+++ Sources/launchPadCore/LaunchpadWindowController.swift
@@ -86,8 +86,7 @@ public final class LaunchpadWindowController: NSObject {
                 // back to the app that was frontmost before the Launchpad.
                 self.close(restoringActivation: false)
                 self.onOpenSettings?()
-            },
-            screenSize: screen.frame.size
+            }
         ))
         hostingView.frame = blurView.bounds
         hostingView.autoresizingMask = [.width, .height]
