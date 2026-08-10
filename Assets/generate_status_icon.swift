fc0210b595a034a942cb12fe59f519d280bad845
Min9Yu3 <ming8969@163.com>
2026-08-10 21:24:16 +0800

    style: reduce status icon corner radii

--- Assets/generate_status_icon.swift
+++ Assets/generate_status_icon.swift
@@ -15,7 +15,7 @@ image.lockFocus()
 NSColor.black.set()
 // Outer rounded-square frame.
 let frameRect = NSRect(x: 5, y: 5, width: 26, height: 26)
-let framePath = NSBezierPath(roundedRect: frameRect, xRadius: 6.5, yRadius: 6.5)
+let framePath = NSBezierPath(roundedRect: frameRect, xRadius: 4.5, yRadius: 4.5)
 framePath.lineWidth = 2
 framePath.stroke()
 
@@ -23,7 +23,7 @@ framePath.stroke()
 let cell: CGFloat = 8
 let gap: CGFloat = 4
 let origin: CGFloat = 8
-let radius: CGFloat = 2
+let radius: CGFloat = 1.5
 for row in 0..<2 {
     for col in 0..<2 {
         let rect = NSRect(
