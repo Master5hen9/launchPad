6d6bda4f22802b85307b1a91234451cf75cb0ce8
Min9Yu3 <ming8969@163.com>
2026-08-22 19:02:30 +0800

    chore: release v0.4.2

--- Sources/launchPadCore/LoginItemInstaller.swift
+++ Sources/launchPadCore/LoginItemInstaller.swift
@@ -76,7 +76,7 @@ public enum LoginItemInstaller {
             "CFBundleExecutable": appName,
             "CFBundleIconFile": "AppIcon",
             "CFBundlePackageType": "APPL",
-            "CFBundleShortVersionString": "0.4.1",
+            "CFBundleShortVersionString": "0.4.2",
             "CFBundleVersion": "1",
             "LSMinimumSystemVersion": "26.0",
             "NSHighResolutionCapable": true
